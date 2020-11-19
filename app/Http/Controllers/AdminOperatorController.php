f468c15089c5795aecc031c422ad788a1ee40ed9
SinSin <whoamiclay021@gmail.com>
2020-11-19 13:52:16 +0330

    dsadsad

--- app/Http/Controllers/AdminOperatorController.php
+++ app/Http/Controllers/AdminOperatorController.php
@@ -160,7 +160,9 @@ class AdminOperatorController extends Controller
             'Chat' => $Chats
         ],200);
     }
-    public function ChangeChatStatus(Request $request){
+
+    public function ChangeChatStatus(Request $request)
+    {
         if ($request->ID && $request->UserID) {
             $Chatsssss = AdminChat::where('UserID', $request->UserID)->where('ReceiverID', $request->ID)->get();
 
@@ -170,14 +172,15 @@ class AdminOperatorController extends Controller
             }
             return response()->json([
                 'msg' => 'Done'
-            ],200);
-        }else{
+            ], 200);
+        } else {
             return response()->json([
                 'msg' => 'Error'
-            ],200);
+            ], 200);
 
         }
     }
+
 
 
 
