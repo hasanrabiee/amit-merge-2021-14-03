18fe7000425ec62c1a074858506148dbfa76f8cc
whoami021 <whoamiclay021@gmail.com>
2020-11-24 22:39:22 +0330

    Adadsdwdwdwd

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -389,8 +389,6 @@ class ApiController extends Controller
         $BoothAFinal = [];
         foreach ($BoothA as $item) {
 
-            \Log::info($item);
-
             if (booth::where('Position' , $item)->where('Hall' , 1)->count() > 0){
                 if (booth::where('Position' , $item)->where('Hall' , 1)->first()->User->AccountStatus == 'Active'){
                     $temp_booth = booth::where('Position' , $item)->where('Hall' , 1)->get()[0];
@@ -404,7 +402,7 @@ class ApiController extends Controller
                     $BoothAFinal[] = 1;
                 }
             }else{
-                $BoothAFinal[] = "jafar $item";
+                $BoothAFinal[] = booth::where('Position' , $item)->where('Hall' , 1)->get()->first();
             }
         }
         $BoothAFinal = array_values($BoothAFinal);
