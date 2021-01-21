3666d6c73517b9b493bb09d542993ad5bd9c6c5e
whoami021 <whoamiclay021@gmail.com>
2021-01-21 21:55:36 +0330

    d

--- config/zoom.php
+++ config/zoom.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'api_key' => env('ZOOM_CLIENT_KEY'),
-    'api_secret' => env('ZOOM_CLIENT_SECRET'),
+    'api_key' => 'qtQbNWKQQIWUyUaVizLd5Q',
+    'api_secret' => '1GQ4rsf2PVtsmQ2TSOnztOWwT2wJA3z7HXsD',
     'base_url' => 'https://api.zoom.us/v2/',
     'token_life' => 60 * 60 * 24 * 7, // In seconds, default 1 week
     'authentication_method' => 'jwt', // Only jwt compatible at present but will add OAuth2
